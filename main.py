81c01cf139dcf0183b90b7e4fe32928728e05f1c
sarabirkby <sbirkby@unomaha.edu>
2024-04-09 14:04:06 -0500

    removed useless code

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import pygame
 import os
-import math
 import threading
 import time
 
-REAL_RANGE = [-2., 1.]
-IMAGINARY_RANGE = [-1., 1.]
+REAL_RANGE: tuple[float, float] = (-2., 1.)
+IMAGINARY_RANGE: tuple[float, float] = (-1., 1.)
 
-WIN_WIDTH = 1000
-WIN_HEIGHT = 700
-
-WIN_ORIGIN_X = 100
-WIN_ORIGIN_Y = 100
+WIN_WIDTH: int = 1000
+WIN_HEIGHT: int = 700
 
 MAX_ITER = 50
 THRESHOLD = 2.
 
 NUM_SHADES = 18
-COLOR_INTENSITY = 175   # Keep at or below 255!
+COLOR_INTENSITY = 175  # Keep at or below 255!
 
-BOX_WIDTH = 2   # Cursor drag box border width in pixels.
+BOX_WIDTH = 2  # Cursor drag box border width in pixels.
 
-MIN_SELECTION_SIZE = 10 # Minimum number of pixels a selected box can be
+MIN_SELECTION_SIZE = 10  # Minimum number of pixels a selected box can be
 
 NUM_THREADS = 8
+
 
 def time_test(func):
     def wrapper(*args):
@@ -31,13 +28,15 @@ def time_test(func):
         retval = func(*args)
         print(f'Time elapsed: {(time.time() - start)} s')
         return retval
+
     return wrapper
 
-def get_iter_val(real_range: list[float, float], imaginary_range: list[float, float]) -> int:
+
+def get_iter_val(real_range: tuple[float, float], imaginary_range: list[float, float]) -> int:
     real_len = real_range[1] - real_range[0]
     imaginary_len = imaginary_range[1] - imaginary_range[0]
     window_area = real_len * imaginary_len
-    iter_val = 75 + int(1 / window_area) + int(1e-3/window_area**2)
+    iter_val = 75 + int(1 / window_area) + int(1e-3 / window_area ** 2)
     return iter_val
 
 
@@ -64,8 +63,7 @@ def print_cursor_rect(win: pygame.Surface, rect: pygame.Rect):
 
 def get_chunk_pixel_colors(chunk, win_width: int, win_height: int, num_iter: int, real_range: tuple[float, float],
                            imaginary_range: tuple[float, float], thread_num: int):
-
-    dh = win_height // NUM_THREADS # height of chunk that each thread will compute
+    dh = win_height // NUM_THREADS  # height of chunk that each thread will compute
     for h in range(0, dh):
         y_val = thread_num * dh + h
         for w in range(win_width):
@@ -79,6 +77,7 @@ def get_all_pixel_colors(win_width: int, win_height: int, num_iter: int, real_ra
     threads: list[None | threading.Thread] = [None] * NUM_THREADS
     dh = win_height // NUM_THREADS  # height of chunk that each thread will compute
     dh_carry = win_height % NUM_THREADS
+
     for t in range(NUM_THREADS):
         chunks[t] = [[None] * win_width] * (dh + dh_carry) if t == NUM_THREADS - 1 else [[None] * win_width] * dh
         threads[t] = threading.Thread(target=get_chunk_pixel_colors,
@@ -86,6 +85,7 @@ def get_all_pixel_colors(win_width: int, win_height: int, num_iter: int, real_ra
         threads[t].start()
     for thread in threads:
         thread.join()
+
     colors = []
     for chunk in chunks:
         colors += chunk
@@ -98,9 +98,9 @@ def get_pixel_complex(x: int, y: int, win_width: int, win_height: int,
     imaginary_range_len = imaginary_range[1] - imaginary_range[0]
 
     # change in real val with 1 pixel difference in x
-    dr = real_range_len / win_width
+    dr: float = real_range_len / win_width
     # change in imaginary val with 1 pixel difference in y
-    di = imaginary_range_len / win_height
+    di: float = imaginary_range_len / win_height
 
     complex_val: complex = complex(dr * x + real_range[0], di * (win_height - y) + imaginary_range[0])
 
@@ -108,7 +108,7 @@ def get_pixel_complex(x: int, y: int, win_width: int, win_height: int,
 
 
 def get_mandel_iter_num(c: complex, max_iter: int):
-    z = 0 + 0j
+    z: complex = 0 + 0j
     for i in range(max_iter):
         z = z ** 2 + c
         if abs(z) >= THRESHOLD:
@@ -130,7 +130,8 @@ def get_mandel_color(x: int, y: int, win_width: int, win_height: int, max_iter: 
 
     current_color_val = int(current_shade * dc)
     red: int = COLOR_INTENSITY - current_color_val
-    green: int = current_color_val*2 if current_color_val < (COLOR_INTENSITY+1)//2 else (COLOR_INTENSITY-current_color_val)*2
+    green: int = current_color_val * 2 if current_color_val < (COLOR_INTENSITY + 1) // 2 else (
+                                                                                                          COLOR_INTENSITY - current_color_val) * 2
     blue: int = current_color_val
 
     return red, green, blue
@@ -149,7 +150,6 @@ def window_init(win, colors):
 
 def main():
     pygame.init()
-    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (WIN_ORIGIN_X, WIN_ORIGIN_Y)
     window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     real_range = REAL_RANGE
     imaginary_range = IMAGINARY_RANGE
@@ -178,7 +178,6 @@ def main():
                 first_pos = pygame.mouse.get_pos()
                 cursor_rect = pygame.Rect(first_pos, (0, 0))
 
-
             if button_down and event.type == pygame.MOUSEBUTTONUP:
                 button_down = False
                 old_colors.append(pixel_colors)
@@ -188,9 +187,9 @@ def main():
                 current_y = pygame.mouse.get_pos()[1]
 
                 x_min = max(min(current_x, first_pos[0]), 0)
-                x_max = min(max(current_x, first_pos[0], x_min+MIN_SELECTION_SIZE), window.get_width())
+                x_max = min(max(current_x, first_pos[0], x_min + MIN_SELECTION_SIZE), window.get_width())
                 y_min = max(min(current_y, first_pos[1]), 0)
-                y_max = min(max(current_y, first_pos[1], x_min+MIN_SELECTION_SIZE), window.get_height())
+                y_max = min(max(current_y, first_pos[1], x_min + MIN_SELECTION_SIZE), window.get_height())
 
                 min_val: complex = get_pixel_complex(x_min, y_min, WIN_WIDTH, WIN_HEIGHT, real_range, imaginary_range)
                 max_val: complex = get_pixel_complex(x_max, y_max, WIN_WIDTH, WIN_HEIGHT, real_range, imaginary_range)
@@ -205,8 +204,6 @@ def main():
 
                 pixel_colors = get_all_pixel_colors(WIN_WIDTH, WIN_HEIGHT, iter_val, real_range, imaginary_range)
                 window_init(window, pixel_colors)
-
-
 
             if button_down and event.type == pygame.MOUSEMOTION:
                 current_x = pygame.mouse.get_pos()[0]
